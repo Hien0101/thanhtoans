7d5693a8559f4b0a1cd3c5121559116eb3dfbcba
Hien0101 <22a1201d0091@students.hou.edu.vn>
2025-07-07 10:22:54 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -29,4 +29,74 @@ twotitleMuangay.addEventListener("click", function (e) {
     addresssever1error.textContent = "Viu lòng nhập địa chỉ của bạn ";
     return;
   }
+  alert("bạn đã mua hàng thành công ");
+});
+const twotitleReturn = document.querySelectorAll(".twotitle-return");
+twotitleReturn.forEach(function (element) {
+  element.addEventListener("click", function () {
+    window.location.href = "https://thotrangcute.github.io/rimuro/";
+  });
+});
+/*
+<!-- Trang https://b.com -->
+<script>
+  window.addEventListener("message", (event) => {
+    // Kiểm tra nguồn gửi đến
+    if (event.origin !== "https://a.com") return;
+
+    const receivedData = event.data;
+    console.log("Dữ liệu nhận được từ a.com:", receivedData);
+
+    // Lưu vào localStorage (nếu muốn)
+    localStorage.setItem("receivedFromA", JSON.stringify(receivedData));
+
+      // Hiển thị ra giao diện
+      const displayDiv = document.getElementById("display");
+      displayDiv.innerHTML = `
+        <p><strong>Tên:</strong> ${receivedData.name}</p>
+        <p><strong>Tin nhắn:</strong> ${receivedData.message}</p>
+  });
+
+</script>
+ window.addEventListener("message", (event) => {
+      // Kiểm tra đúng nguồn gốc trang A
+      if (event.origin !== "https://a.com") return;
+
+      const receivedData = event.data;
+
+      // Hiển thị ra giao diện
+      const displayDiv = document.getElementById("display");
+      displayDiv.innerHTML = `
+        <p><strong>Tên:</strong> ${receivedData.name}</p>
+        <p><strong>Tin nhắn:</strong> ${receivedData.message}</p>
+      `;
+*/
+window.addEventListener("cartItem", (event) => {
+  if (event.origin !== "https://thotrangcute.github.io/rimuro/") return;
+  const receivedData = event.data;
+  console.log(
+    "Dữ liệu nhận được từ https://thotrangcute.github.io/rimuro/ :",
+    receivedData
+  );
+  localStorage.setItem("receivedFromA", JSON.stringify(receivedData));
+  const hiddenProducts = document.getElementById("hiddenProducts");
+  hiddenProducts.innerHTML = `
+      <div class="item-content">
+      <img src="${receivedData.images}" alt="${receivedData.name}" class="cart-item-img" />
+      <div class="cart-item-info">
+      <h4>${receivedData.name}</h4>
+      <p>${receivedData.price}</p>
+      <div class="item-counter">
+          <button class="decrease">-</button>
+          <div class="value">${receivedData.quantity}</div>
+          <button class="increase">+</button>
+      </div>
+      <div class="item-message"></div>
+      </div>
+      <div class="cart-item-wash">
+      <i class="fa fa-trash-o"></i>
+      </div>
+
+      </div>
+  `;
 });
